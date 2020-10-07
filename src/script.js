fceb1c8eaf19fe1459322cc65547ae03bbb08292
faiza203 <fiazfaiza981@gmail.com>
2020-10-07 16:55:37 +0500

    Create .babelrc

--- src/script.js
+++ src/script.js
@@ -2,3 +2,5 @@ import "./app.js";
 import React from "react";
 import ReactDOM from "react-dom"
 console.log(React , ReactDOM);
+const para = <p>I am JSX from webpack</p>
+ReactDOM.render(para , app)
