8207996c6ac9bd9497e17a848770eccf8a585a16
faiza203 <fiazfaiza981@gmail.com>
2020-10-10 17:47:55 +0500

    Rename nomatch.js to noFound.js

---
+++ src/components/routes/dashboard.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+const expenseDashboardPage = () => (<div>Expense Dashboard Page</div>);
+export default expenseDashboardPage ;
---
+++ src/components/routes/routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Header from "./header";
+import addExpensePage from "./create"
+import expenseDashboardPage from "./dashboard";
+import helpPage from "./help";
+import editPage from "./edit";
+import noFoundPage from "./noFound";
+const AapRoutes  = () =>(
+      <BrowserRouter>
+            <Header />
+           
+      </BrowserRouter>
+)
+export default AapRoutes;
+// <Switch>
+// <Route path="/" component={expenseDashboardPage} exact={true} />
+// <Route path="/create" component={addExpensePage} />
+// <Route path="/help" component={helpPage} />
+// <Route path="/edit" component={editPage} />
+// <Route component={noFoundPage} />
+// </Switch>
