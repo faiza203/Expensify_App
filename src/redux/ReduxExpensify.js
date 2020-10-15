4237c78d0d4bd0d3de1fab113184ebe164001853
faiza203 <fiazfaiza981@gmail.com>
2020-10-15 15:50:09 +0500

    Create expenses.js

--- src/redux/ReduxExpensify.js
+++ src/redux/ReduxExpensify.js
@@ -63,15 +63,6 @@ const setEndDate = (endDate) => ({
 });
 
 
-// Store creation
-
-const store = createStore(
-  combineReducers({
-    expenses: expensesReducer,
-    filters: filtersReducer
-  })
-);
-
 store.subscribe(() => {
   const state = store.getState();
   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
