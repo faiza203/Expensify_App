925a5475ac0bbd84f851a6ec296f63042046e72c
faiza203 <fiazfaiza981@gmail.com>
2020-10-15 16:36:18 +0500

    import some js files

--- src/script.js
+++ src/script.js
@@ -2,5 +2,13 @@ import "./../styles/style.scss";
 import React from "react";
 import ReactDOM from "react-dom"
 import AapRoutes from "./RoutesComponents/routes";
-import store from "./store/expenses";
+import {addExpense, addExpenses , setTextFilters} from "./action/expenses";
+import giveVisibleExpenses from "./selectors/expenses";
+import configureStore from "./store/configureStore.js";
+
+const store = configureStore();
+store.dispatch(addExpense({description : "Water Bill"}));;
+const state = store.getState();
+const visibleExpenses = giveVisibleExpenses(state.expenses , state.filters);
+console.log(visibleExpenses);
 ReactDOM.render(<AapRoutes />, document.getElementById("app"))
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,11 +1,10 @@
 
 // Get visible expenses
-export default(expenses, { text, sortBy, startDate, endDate }) => {
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
       const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
       const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
       const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-  
       return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
       if (sortBy === 'date') {
@@ -15,4 +14,4 @@ export default(expenses, { text, sortBy, startDate, endDate }) => {
       }
     });
   };
-  
+export default getVisibleExpenses;
