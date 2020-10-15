5b1c743b23321c068ca835d32fd4a384dba5ead9
faiza203 <fiazfaiza981@gmail.com>
2020-10-15 15:52:01 +0500

    Create action/filter.js

--- src/redux/ReduxExpensify.js
+++ src/redux/ReduxExpensify.js
@@ -2,38 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import { v4 as uuid } from 'uuid';
 import "./../../styles/style.scss";
 
-// ADD_EXPENSE
-const addExpense = (
-  {
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
-  type: 'ADD_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
-});
-
-// REMOVE_EXPENSE
-const removeExpense = ({ id } = {}) => ({
-  type: 'REMOVE_EXPENSE',
-  id
-});
-
-// EDIT_EXPENSE
-const editExpense = (id, updates) => ({
-  type: 'EDIT_EXPENSE',
-  id,
-  updates
-});
-
 // SET_TEXT_FILTER
 const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
