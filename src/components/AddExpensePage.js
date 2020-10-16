1e4e611124d2b55a55b4b45b8dd4cce223dfcbcd
faiza203 <fiazfaiza981@gmail.com>
2020-10-16 17:00:05 +0500

    Create dates div

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
-    This is from my add expense component
+    <h1>Add Expense</h1>
+    <ExpenseForm
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push('/');
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
--- src/components/expenseListItem.js
+++
@@ -1,7 +0,0 @@
-const expenseListItem = ({description , amount , createAt}) => (
-  <div>
-  <h3>{description}</h3>
-  <p>{amount} - {createAt}</p>
-  </div>
-);
-export default expenseListItem;
