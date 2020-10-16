cc08607e2cbe75e723ad154c9808ac2783e5d038
faiza203 <fiazfaiza981@gmail.com>
2020-10-16 17:19:59 +0500

    Add creating new expense function

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,14 +1,7 @@
 import {v4 as uuid} from 'uuid';
 
 // ADD_EXPENSE
-export const addExpense = (
-  {
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
+export const addExpense = ({ description = '',   note = '', amount = 0,    createdAt = 0 } = {}) => ({
   type: 'ADD_EXPENSE',
   expense: {
     id: uuid(),
